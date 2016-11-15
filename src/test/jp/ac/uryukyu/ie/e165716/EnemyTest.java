b0eccb327f219dda2379f88ae233141a8d4c8e75
ryota uehara <e165716@ie.u-ryukyu.ac.jp>
2016-11-15 18:47:24 +0900

    MATCH X1

---
+++ src/test/jp/ac/uryukyu/ie/e165716/EnemyTest.java
@@ -0,0 +1,21 @@
+package jp.ac.uryukyu.ie.e165716;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+public class EnemyTest {
+    @Test
+    public void attack() throws Exception {
+        int heroHP = 10;
+        Hero hero = new Hero("ヨシヒコ", heroHP, 5);
+        Enemy enemy = new Enemy("スライム", 6, 3);
+        enemy.dead = true;
+
+        for(int i=0; i<10; i++){
+            enemy.attack(hero);
+        }
+        assertEquals(heroHP, hero.hitPoint);
+
+    }
+
+}
