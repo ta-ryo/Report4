b0dfbeb7e3c9c0ce3cf32af53542e2d79fb66260
ryota uehara <e165716@ie.u-ryukyu.ac.jp>
2016-11-18 15:31:46 +0900

    MATCH X1

--- src/main/jp/ac/uryukyu/ie/e165716/Enemy.java
+++ src/main/jp/ac/uryukyu/ie/e165716/Enemy.java
@@ -1,62 +1,67 @@
 package jp.ac.uryukyu.ie.e165716;
 
 public class Enemy {
-        String name;
-        int hitPoint;
-        int attack;
-        boolean dead;
+    String name;
+    int hitPoint;
+    int attack;
+    boolean dead;
 
-        /**
-         * コンストラクタ。名前、最大HP、攻撃力を指定する。
-         * @param name モンスター名
-         * @param maximumHP モンスターのHP
-         * @param attack モンスターの攻撃力
-         */
-        public Enemy (String name, int maximumHP, int attack) {
-            this.name = name;
-            hitPoint = maximumHP;
-            this.attack = attack;
-            dead = false;
-            System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
-        }
+    /**
+     * コンストラクタ。名前、最大HP、攻撃力を指定する。
+     *
+     * @param name      モンスター名
+     * @param maximumHP モンスターのHP
+     * @param attack    モンスターの攻撃力
+     */
+    public Enemy(String name, int maximumHP, int attack) {
+        this.name = name;
+        hitPoint = maximumHP;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
+    }
 
-        /**
-         * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
-         * @return boolean
-         */
-        public boolean isDead() {
-            return dead;
-        }
+    /**
+     * getterメソッドと同等。生死をboolean表現しているためメソッド名をisDead()とした。
+     *
+     * @return boolean
+     */
+    public boolean isDead() {
+        return dead;
+    }
 
-        public String getName(){
-            return name;
-        }
+    public String getName() {
+        return name;
+    }
 
-        /**
-         * Heroへ攻撃するメソッド。
-         * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
-         * @param hero 攻撃対象
-         */
-        public void attack(Hero hero,int maximumHP) {
-            if(maximumHP>0) {
-                int damage = (int) (Math.random() * attack);
-                System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
-                hero.wounded(damage);
-            }
-            else{
-                int damage = 0;
-            }
+    /**
+     * Heroへ攻撃するメソッド。
+     * attackに応じて乱数でダメージを算出し、hero.wounded()によりダメージ処理を実行。
+     *
+     * @param hero 攻撃対象
+     */
+    public void attack(Hero hero) {
+        if (hitPoint > 0) {
+            int damage = (int) (Math.random() * attack);
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, hero.getName(), damage);
+            hero.wounded(damage);
         }
+        else {
+            System.out.printf("%sは死んでいる\n",name);
+        }
+    }
 
-        /**
-         * 自身へ攻撃されたときのダメージ処理をするメソッド。
-         * 指定されたダメージを hitPoint から引き、死亡判定を行う。
-         * @param damage 受けたダメージ
-         */
-        public void wounded(int damage) {
-            hitPoint -= damage;
-            if (hitPoint < 0) {
-                dead = true;
-                System.out.printf("モンスター%sは倒れた。\n", name);
-            }
+    /**
+     * 自身へ攻撃されたときのダメージ処理をするメソッド。
+     * 指定されたダメージを hitPoint から引き、死亡判定を行う。
+     *
+     * @param damage 受けたダメージ
+     */
+    public void wounded(int damage) {
+        hitPoint -= damage;
+        if (hitPoint < 0) {
+            dead = true;
+            System.out.printf("モンスター%sは倒れた。\n", name);
         }
+    }
+}
